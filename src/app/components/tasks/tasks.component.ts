60d15cd8121f14c06bcb751f60cbb6faeadc6231
Avaaren <avaaren@mail.ru>
2020-04-28 17:20:43 +0300

    Added get color method. Added mat-table data source declaration

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DataHandlerService } from 'src/app/services/data-handler.service';
 import { Task } from 'src/app/models/Task';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-tasks',
@@ -11,13 +12,33 @@ export class TasksComponent implements OnInit {
 
   tasks: Task[];
 
+  columnsToDisplay: string[] = ['color', 'position', 'name', 'category', 'priority', 'date'];
+  dataSource: MatTableDataSource<Task>;
+
   constructor(private dataHandler: DataHandlerService) { }
 
   ngOnInit() {
-    this.dataHandler.taskSubject.subscribe( tasks => this.tasks = tasks);
+    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+
+    this.dataSource = new MatTableDataSource();
+
+    this.refreshTable();
   }
 
-  toggleTaskCompleted(task: Task){
+  getPriorityColor(task: Task) {
+    if (task.priority && task.priority.color ){
+      return task.priority.color
+    }
+    else {
+      return '#fff'
+    }
+  }
+
+  toggleTaskCompleted(task: Task) {
     task.is_completed = !task.is_completed;
+  }
+
+  refreshTable() {
+    this.dataSource.data = this.tasks;
   }
 }
