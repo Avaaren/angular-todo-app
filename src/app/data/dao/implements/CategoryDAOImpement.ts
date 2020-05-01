6be76f14dfb6a96141acc0535b962f2dad5f0d25
Avaaren <avaaren@mail.ru>
2020-05-01 14:44:29 +0300

    Added method for getting all ctegories

--- src/app/data/dao/implements/CategoryDAOImpement.ts
+++ src/app/data/dao/implements/CategoryDAOImpement.ts
@@ -1,27 +1,35 @@
 import { CategoryDAO } from '../interfaces/CategoryDAO';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Category } from 'src/app/models/Category';
-import { Priority } from 'src/app/models/Priority';
-import { Task } from 'src/app/models/Task';
+import { TestData } from '../../TestData';
 
 export class CategoryDAOImplement implements CategoryDAO {
+
     find(query: string): Observable<Category[]> {
         throw new Error("Method not implemented.");
     }
+
+
     add(T: any): Observable<Category> {
         throw new Error("Method not implemented.");
     }
+
+
     get(id: number): Observable<Category> {
         throw new Error("Method not implemented.");
     }
     update(id: number): Observable<Category> {
         throw new Error("Method not implemented.");
     }
+
+
     delete(id: number): Observable<Category> {
         throw new Error("Method not implemented.");
     }
+
+
     getAll(): Observable<Category[]> {
-        throw new Error("Method not implemented.");
+        return of(TestData.categories);
     }
     
 }
