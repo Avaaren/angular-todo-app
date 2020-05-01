bac6977861d506514b29eb715364ae6d62b8354c
Avaaren <avaaren@mail.ru>
2020-05-01 14:44:08 +0300

    Added methods for get all tasks and tasks by category

--- src/app/data/dao/implements/TaskDAOImpement.ts
+++ src/app/data/dao/implements/TaskDAOImpement.ts
@@ -1,14 +1,31 @@
 import { TaskDAO } from '../interfaces/TaskDAO';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { Category } from 'src/app/models/Category';
 import { Priority } from 'src/app/models/Priority';
 import { Task } from 'src/app/models/Task';
+import { TestData } from '../../TestData';
 
 
 export class TaskDAOImplement implements TaskDAO {
+
+    // We have method that get query from datahandler with some data and
+    // And then it throws it to inner method...
+
+    // After inner method data return, main method returns observble task array
     find(query: string, category: Category, priority: Priority, status: boolean): Observable<Task[]> {
-        throw new Error("Method not implemented.");
+        return of(this.seacrhTask(query, category, priority, status));
     }
+    // And that inner method filtering task array based on got data
+    private seacrhTask(query: string, category: Category, priority: Priority, status: boolean){
+        let allTasks = TestData.tasks;
+
+        if (category != null){
+           allTasks = allTasks.filter(task => task.category === category);
+            return allTasks;
+        }
+        return allTasks;
+    }
+
     getCompletedCountInCategory(category: Category): Observable<number> {
         throw new Error("Method not implemented.");
     }
@@ -34,7 +51,7 @@ export class TaskDAOImplement implements TaskDAO {
         throw new Error("Method not implemented.");
     }
     getAll(): Observable<Task[]> {
-        throw new Error("Method not implemented.");
+        return of(TestData.tasks);
     }
 
 }
