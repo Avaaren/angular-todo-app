41079bd24fa2a66a19a931614ac8d568d238bc98
Avaaren <avaaren@mail.ru>
2020-05-02 13:16:32 +0300

    Added logic for 2w binding category and priority editing and their edition

--- src/app/components/dialog/edit-task-dialog/edit-task-dialog.component.ts
+++ src/app/components/dialog/edit-task-dialog/edit-task-dialog.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { Task } from 'src/app/models/Task';
 import { DataHandlerService } from 'src/app/services/data-handler.service';
+import { Category } from 'src/app/models/Category';
+import { Priority } from 'src/app/models/Priority';
 
 @Component({
   selector: 'app-edit-task-dialog',
@@ -22,17 +24,32 @@ export class EditTaskDialogComponent implements OnInit {
 
     dialogTitle: string;
     task: Task;
+
     tmpTitle: string;
+    tmpCategory: Category;
+    tmpPriority: Priority;
+
+    categories: Category[];
+    priorities: Priority[];
 
   ngOnInit(): void {
     // Passing into variables data, passed from parent component
     this.dialogTitle = this.data[1];
     this.task = this.data[0];
+
     this.tmpTitle = this.task.title;
+    this.tmpCategory = this.task.category;
+    this.tmpPriority = this.task.priority;
+
+    this.dataHandler.getAllCategories().subscribe(categories => this.categories = categories);
+    this.dataHandler.getAllPriorities().subscribe(priorities => this.priorities = priorities);
   }
 
   onConfirm(){
     this.task.title = this.tmpTitle;
+    this.task.category = this.tmpCategory;
+    this.task.priority = this.tmpPriority;
+
     this.dialogRef.close(this.task)
   }
 
