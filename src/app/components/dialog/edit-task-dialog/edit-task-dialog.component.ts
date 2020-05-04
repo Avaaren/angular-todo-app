2072e70cda7ac1df24195f3fb5ddf84c23351763
Avaaren <avaaren@mail.ru>
2020-05-04 13:27:59 +0300

    Added variable for 2w binding with calendar

--- src/app/components/dialog/edit-task-dialog/edit-task-dialog.component.ts
+++ src/app/components/dialog/edit-task-dialog/edit-task-dialog.component.ts
@@ -28,6 +28,7 @@ export class EditTaskDialogComponent implements OnInit {
   tmpTitle: string;
   tmpCategory: Category;
   tmpPriority: Priority;
+  tmpDate: any;
 
   categories: Category[];
   priorities: Priority[];
@@ -36,20 +37,24 @@ export class EditTaskDialogComponent implements OnInit {
     // Passing into variables data, passed from parent component
     this.dialogTitle = this.data[1];
     this.task = this.data[0];
-
+    //Todo: Date component throw undefined errors in console!!!!
     this.tmpTitle = this.task.title;
     this.tmpCategory = this.task.category;
     this.tmpPriority = this.task.priority;
+    this.tmpDate = this.task.date;
 
     this.dataHandler.getAllCategories().subscribe(categories => this.categories = categories);
     this.dataHandler.getAllPriorities().subscribe(priorities => this.priorities = priorities);
+
   }
 
   onConfirm() {
     this.task.title = this.tmpTitle;
     this.task.category = this.tmpCategory;
     this.task.priority = this.tmpPriority;
-
+    this.task.date = this.tmpDate;
+    
+    
     this.dialogRef.close(this.task)
   }
 
@@ -68,7 +73,7 @@ export class EditTaskDialogComponent implements OnInit {
     // When window is closed - this method runs and get result. 
     // We can make some actions or handle result (what we return from dialog window) in {}
     dialogRef.afterClosed().subscribe(result => {
-      if (result == 'true') {
+      if (result == true) {
         this.dialogRef.close('delete');
         return;
       }
